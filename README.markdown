2a92ebccc989e32d3aaab10aa85c152c6ad01da8
Hexen <1483bk@mail.ru>
2011-05-31 19:32:19 +0400

    End

---
+++ README.markdown
@@ -0,0 +1,4 @@
+# Учебник Ruby on Rails: пример приложения
+  Это пример приложения для
+  [*Учебник Ruby on Rails: Изучаем Rails на Примерах*](http://railstutorial.org/)
+  by [Michael Hartl](http://michaelhartl.com/).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '9611e924c1209a34cd55a331358d609054b3393af4147d767e1be641ecbcaa0d7d32e25ca9d4b3dcdc50888018c3e8b266dff016d515386363adea210c24f102'
